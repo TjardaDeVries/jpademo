679f224de8c6c4c3a207087da15d6ac52554fe1c
Tjarda de Vries <tjardadevries@BA92-2TG33YGTFL.local>
2018-05-28 09:14:34 +0200

    Adds findAll method

--- src/main/java/nl/ordina/jpademo/persistence/PersonRepo.java
+++ src/main/java/nl/ordina/jpademo/persistence/PersonRepo.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class PersonRepo {
 
@@ -17,5 +19,12 @@ public class PersonRepo {
         jdbcTemplate.execute(statement);
         return person;
     }
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person;",
+                (rs, rowNum) -> new Person(rs.getLong("id"), rs.getString("name"))
+        );
+    }
+
 
 }
